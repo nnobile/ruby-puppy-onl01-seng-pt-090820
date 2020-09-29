76c82692f62ce4b66c78203eda58cf54bc1ea5d9
Nick Nobile <nickcnobile@gmail.com>
2020-09-29 00:22:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Add your code here
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+    puts dog
+  end
+end
+
+
+end
